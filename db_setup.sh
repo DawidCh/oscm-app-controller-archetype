acf83d8336dd7f8da4fd8cf282f2d0c5914933a9
Dawid Chojnacki <dawid.chojnacki@ts.fujitsu.com>
2017-11-27 08:29:52 +0100

    Mirroring repo and cleaning up unneeded projets

--- db_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-psql -c "create user bssappunituser with password 'bssappunituser';" -U postgres;
-psql -c "create user bssappunituser with password 'bssappunituser';" -U postgres;
-psql -c "create database bssappunit with owner bssappunituser TEMPLATE=template0 ENCODING='UTF8'" -U postgres;
-psql -c "create schema bssappunit" -U postgres;
-psql -c "grant all privileges on schema bssappunit to bssappunituser" -U postgres;
